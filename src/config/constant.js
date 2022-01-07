80fe21a9abf442b7f13a5be830afd3817b3710ea
D3NCyan <edwarddoltons@gmail.com>
2022-01-07 16:42:46 +0800

    change all color in DATA. constant.js

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,10 +1,10 @@
 const SLIDER_DATA = [
-  { title: "How to do", color: "lightblue" },
-  { title: "Make your life easy", color: "red" },
-  { title: "How to do", color: "orange" },
-  { title: "Make your life easy", color: "maroon" },
-  { title: "How to do", color: "purple" },
-  { title: "Make your life easy", color: "blue" },
+  { title: "How to do", color: "#008071" },
+  { title: "Make your life easy", color: "#005b80" },
+  { title: "How to do", color: "#002d80" },
+  { title: "Make your life easy", color: "#350080" },
+  { title: "How to do", color: "#660080" },
+  { title: "Make your life easy", color: "#80004f" },
 ];
 
 export { SLIDER_DATA };
