a659c94e6d86d34c381ca22421f3030784e42f94
D3NCyan <edwarddoltons@gmail.com>
2022-01-07 16:34:01 +0800

    remove text and use box height in theme, CoverContent

--- src/components/Home/CoverContent.js
+++ src/components/Home/CoverContent.js
@@ -1,17 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
-
-const boxWidth = 500;
+import { BORDER_TRENDY, BOX_HEIGHT } from "../../config/theme";
 
 export default function CoverContent() {
-  return (
-    <View style={styles.container}>
-      <Text>Make your life simple</Text>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "lightblue",
+    height: BOX_HEIGHT,
+    backgroundColor: "#5447B6",
+    borderRadius: BORDER_TRENDY,
+    marginTop: 10,
   },
 });
