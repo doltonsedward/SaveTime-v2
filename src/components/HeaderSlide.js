a201f281713c2339ac9da3920e3346ee96710abd
D3NCyan <edwarddoltons@gmail.com>
2022-01-07 10:51:03 +0800

    add styling for FlatList in HeaderSlide component

--- src/components/HeaderSlide.js
+++ src/components/HeaderSlide.js
@@ -1,5 +1,6 @@
-import { FlatList, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { SLIDER_DATA } from "../config/constant";
+import { BORDER_TRENDY, ITEM_WIDTH, width, SPACING } from "../config/theme";
 
 export default function HeaderSlide() {
   return (
@@ -7,13 +8,28 @@ export default function HeaderSlide() {
       data={SLIDER_DATA}
       keyExtractor={(item) => item.color}
       horizontal
+      contentContainerStyle={{
+        padding: SPACING,
+      }}
+      decelerationRate={"fast"}
       renderItem={({ item }) => {
         return (
-          <View>
-            <Text>{item.title}</Text>
+          <View style={[styles.itemContainer, { backgroundColor: item.color }]}>
+            <Text style={{ color: "white" }}>{item.title}</Text>
           </View>
         );
       }}
+      showsHorizontalScrollIndicator={false}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    width: width,
+    height: width / 2,
+    borderRadius: BORDER_TRENDY,
+    margin: 5,
+    padding: SPACING,
+  },
+});
