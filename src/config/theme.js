15001b819d313b2f668da40c922c92d7cf3ecd1c
D3NCyan <edwarddoltons@gmail.com>
2022-01-07 17:08:42 +0800

    add fontSize and padding in config/theme

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,8 +1,20 @@
 const width = 150;
+const fontSize = 30;
+const padding = 20;
+
 const BOX_HEIGHT = 150;
 const SPACING = 20;
 const BORDER_TRENDY = 8;
 const BORDER_CIRCLE = 50;
 const FONT = "Poppins";
 
-export { width, BOX_HEIGHT, SPACING, BORDER_TRENDY, BORDER_CIRCLE, FONT };
+export {
+  width,
+  fontSize,
+  padding,
+  BOX_HEIGHT,
+  SPACING,
+  BORDER_TRENDY,
+  BORDER_CIRCLE,
+  FONT,
+};
