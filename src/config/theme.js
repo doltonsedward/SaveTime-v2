1f89ba31d959de2d75134186830035ec6b9d4d57
D3NCyan <edwarddoltons@gmail.com>
2022-01-07 16:15:40 +0800

    increase SPACING and add BORDER_CIRCLE

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,6 +1,7 @@
 const width = 150;
 const ITEM_WIDTH = 400;
-const SPACING = 10;
+const SPACING = 20;
 const BORDER_TRENDY = 8;
+const BORDER_CIRCLE = 50;
 
-export { width, ITEM_WIDTH, SPACING, BORDER_TRENDY };
+export { width, ITEM_WIDTH, SPACING, BORDER_TRENDY, BORDER_CIRCLE };
