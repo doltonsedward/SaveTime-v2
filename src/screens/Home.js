1153146778898c065e8270cc67eba70e50100723
D3NCyan <edwarddoltons@gmail.com>
2022-01-07 08:53:33 +0800

    use HeaderSlide in home page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
+import HeaderSlide from "../components/HeaderSlide";
 
 export default function Home() {
   return (
     <View>
+      <HeaderSlide />
       <Text>Halo dunia</Text>
     </View>
   );
