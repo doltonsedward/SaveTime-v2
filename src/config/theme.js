9684096e75df58a8715776b63475ee136d1a2f60
D3NCyan <edwarddoltons@gmail.com>
2022-01-07 16:42:30 +0800

    change ITEM_WIDTH to BOX_HEIGHT and add FONT in theme

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,7 +1,8 @@
 const width = 150;
-const ITEM_WIDTH = 400;
+const BOX_HEIGHT = 150;
 const SPACING = 20;
 const BORDER_TRENDY = 8;
 const BORDER_CIRCLE = 50;
+const FONT = "Poppins";
 
-export { width, ITEM_WIDTH, SPACING, BORDER_TRENDY, BORDER_CIRCLE };
+export { width, BOX_HEIGHT, SPACING, BORDER_TRENDY, BORDER_CIRCLE, FONT };
