cd41ce236e711a0f6b00315f60c450309135a6d8
D3NCyan <edwarddoltons@gmail.com>
2022-01-09 22:05:07 +0800

    centering input, add padding and white background

--- src/components/atoms/Input.js
+++ src/components/atoms/Input.js
@@ -3,14 +3,24 @@ import { StyleSheet, TextInput, View } from "react-native";
 export default function InputTodo({ placeholder }) {
   return (
     <View style={styles.container}>
-      <TextInput placeholder={placeholder} />
+      <TextInput style={styles.input} placeholder={placeholder} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "row",
+    justifyContent: "center",
     position: "absolute",
     bottom: 10,
+    left: 0,
+    right: 0,
+  },
+  input: {
+    paddingVertical: 10,
+    paddingHorizontal: 8,
+    width: 200,
+    backgroundColor: "#FFF",
   },
 });
